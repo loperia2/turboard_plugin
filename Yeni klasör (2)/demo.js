5b5f4d13d897fe24d8265dc8e4f22f9bada6e325
loperia2 <41861732+loperia2@users.noreply.github.com>
2021-04-16 16:27:28 +0300

    Add files via upload

---
+++ Yeni klasör (2)/demo.js
@@ -0,0 +1,493 @@
+
+var{SideeX}=require('sideex-api');
+var sideex= new SideeX();
+let obj={
+    "time": 1618404591696,
+    "version": {
+      "sideex": [
+        3,
+        2,
+        1
+      ],
+      "format": [
+        2,
+        0,
+        0
+      ]
+    },
+    "suites": [
+      { 
+        "fileName":"Untitled Test Suite.html",
+        "title": "Untitled Test Suite",
+        "cases": [
+          {
+            "title": "Untitled Test Case",
+            "records": [
+             {
+                "name": "mouseOver",
+                "target": {
+                  "usedIndex": 0,
+                  "options": [
+                    {
+                      "type": "xpath:idRelative",
+                      "value": "//div[@id='app']/div[1]/main[1]/div[1]/div[1]/nav[1]/div[2]/div[1]"
+                    },
+                    {
+                      "type": "xpath:position",
+                      "value": "//div[2]/div[1]"
+                    },
+                    {
+                      "type": "css:position",
+                      "value": "css=div.v-list.v-sheet.theme--dark.v-list--dense.v-list--nav"
+                    }
+                  ],
+                  "tac": ""
+                },
+                "value": {
+                  "usedIndex": 0,
+                  "options": [
+                    {
+                      "type": "other",
+                      "value": "45,0"
+                    }
+                  ],
+                  "tac": ""
+                },
+            
+              },
+              {
+                "name": "mouseOver",
+                "target": {
+                  "usedIndex": 0,
+                  "options": [
+                    {
+                      "type": "xpath:idRelative",
+                      "value": "//div[@id='app']/div[1]/main[1]/div[1]/div[1]/nav[1]/div[2]/div[1]/div[1]/a[1]"
+                    },
+                    {
+                      "type": "xpath:position",
+                      "value": "//div[1]/div[1]/a[1]"
+                    },
+                    {
+                      "type": "xpath:href",
+                      "value": "//a[@href='/dashlets/']"
+                    },
+                    {
+                      "type": "css:position",
+                      "value": "css=a.v-list-item.v-list-item--link.theme--dark.ma-0"
+                    },
+                    {
+                      "type": "link",
+                      "value": "link=Görseller"
+                    }
+                  ],
+                  "tac": ""
+                },
+                "value": {
+                  "usedIndex": 0,
+                  "options": [
+                    {
+                      "type": "other",
+                      "value": "24,1"
+                    }
+                  ],
+                  "tac": ""
+                },
+                
+              },
+              {
+                "name": "mouseOver",
+                "target": {
+                  "usedIndex": 0,
+                  "options": [
+                    {
+                      "type": "xpath:idRelative",
+                      "value": "//div[@id='app']/div[1]/main[1]/div[1]/div[1]/nav[1]/div[2]/div[1]/div[1]/a[1]/div[1]/i[1]"
+                    },
+                    {
+                      "type": "xpath:position",
+                      "value": "//div[1]/i[1]"
+                    },
+                    {
+                      "type": "css:position",
+                      "value": "css=i.v-icon.notranslate.mdi.mdi-chart-box-outline.theme--dark"
+                    }
+                  ],
+                  "tac": ""
+                },
+                "value": {
+                  "usedIndex": 0,
+                  "options": [
+                    {
+                      "type": "other",
+                      "value": "4,0"
+                    }
+                  ],
+                  "tac": ""
+                },
+              
+              },
+              {
+                "name": "clickAt",
+                "target": {
+                  "usedIndex": 0,
+                  "options": [
+                    {
+                      "type": "xpath:idRelative",
+                      "value": "//div[@id='app']/div[1]/main[1]/div[1]/div[1]/nav[1]/div[2]/div[1]/div[1]/div[1]/i[1]"
+                    },
+                    {
+                      "type": "xpath:position",
+                      "value": "//div[1]/div[1]/i[1]"
+                    },
+                    {
+                      "type": "css:position",
+                      "value": "css=i.v-icon.notranslate.mdi.mdi-plus.theme--dark"
+                    }
+                  ],
+                  "tac": ""
+                },
+                "value": {
+                  "usedIndex": 0,
+                  "options": [
+                    {
+                      "type": "other",
+                      "value": "9,4"
+                    }
+                  ],
+                  "tac": ""
+                },
+             
+              },
+              {
+                "name": "clickAt",
+                "target": {
+                  "usedIndex": 0,
+                  "options": [
+                    {
+                      "type": "xpath:idRelative",
+                      "value": "//div[@id='app']/div[3]/div[1]/div[1]/div[1]/form[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]"
+                    },
+                    {
+                      "type": "xpath:position",
+                      "value": "//div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]"
+                    },
+                    {
+                      "type": "css:position",
+                      "value": "css=div.v-list-item__content > div.v-list-item__title"
+                    }
+                  ],
+                  "tac": ""
+                },
+                "value": {
+                  "usedIndex": 0,
+                  "options": [
+                    {
+                      "type": "other",
+                      "value": "68,4"
+                    }
+                  ],
+                  "tac": ""
+                },
+              
+              },
+              {
+                "name": "clickAt",
+                "target": {
+                  "usedIndex": 1,
+                  "options": [
+                    {
+                      "type": "id",
+                      "value": "id=input-796"
+                    },
+                    {
+                      "type": "xpath:idRelative",
+                      "value": "//div[@id='app']/div[4]/div[1]/div[1]/div[1]/div[1]/div[1]/input[1]"
+                    },
+                    {
+                      "type": "xpath:position",
+                      "value": "//div[4]/div[1]/div[1]/div[1]/div[1]/div[1]/input[1]"
+                    },
+                    {
+                      "type": "xpath:attributes",
+                      "value": "//input[@id='input-796']"
+                    },
+                    {
+                      "type": "css:position",
+                      "value": "css=#input-796"
+                    }
+                  ],
+                  "tac": ""
+                },
+                "value": {
+                  "usedIndex": 0,
+                  "options": [
+                    {
+                      "type": "other",
+                      "value": "107,0"
+                    }
+                  ],
+                  "tac": ""
+                },
+             
+              },
+              {
+                "name": "sendKeys",
+                "target": {
+                  "usedIndex": 1,
+                  "options": [
+                    {
+                      "type": "id",
+                      "value": "id=input-796"
+                    },
+                    {
+                      "type": "xpath:idRelative",
+                      "value": "//div[@id='app']/div[4]/div[1]/div[1]/div[1]/div[1]/div[1]/input[1]"
+                    },
+                    {
+                      "type": "xpath:position",
+                      "value": "//div[4]/div[1]/div[1]/div[1]/div[1]/div[1]/input[1]"
+                    },
+                    {
+                      "type": "xpath:attributes",
+                      "value": "//input[@id='input-796']"
+                    },
+                    {
+                      "type": "css:position",
+                      "value": "css=#input-796"
+                    }
+                  ],
+                  "tac": ""
+                },
+                "value": {
+                  "usedIndex": 0,
+                  "options": [
+                    {
+                      "type": "other",
+                      "value": "Kickstarter *"
+                    }
+                  ],
+                  "tac": ""
+                },
+               
+              },
+              {
+                "name":"dragAndDrop"/*"clickAt"*/,
+                "target": {
+                  "usedIndex": 1,
+                  "options": [
+                    {
+                      "type": "xpath:idRelative",
+                      "value": "//div[@id='list-item-836']/div[1]/div[1]"
+                    },
+                    {
+                      "type": "xpath:position",
+                      "value": "//div[4]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]"
+                    },
+                    {
+                      "type": "css:position",
+                      "value": "css=#list-item-836 > div.v-list-item__content > div.v-list-item__title"
+                    }
+                  ],
+                  "tac": ""
+                },
+                "value": {
+                  "usedIndex": 0,
+                  "options": [
+                    {
+                      "type": "other",
+                      "value": "{\"StartPoint\":{\"X\":107,\"Y\":6},\"Movements\":[{\"TD\":7,\"OX\":0,\"OY\":1}]}"
+                    }
+                  ],
+                  "tac": ""
+                },
+               
+              },
+              {
+                "name": "clickAt",
+                "target": {
+                  "usedIndex": 1,
+                  "options": [
+                    {
+                      "type": "id",
+                      "value": "id=input-778"
+                    },
+                    {
+                      "type": "xpath:idRelative",
+                      "value": "//div[@id='app']/div[3]/div[1]/div[1]/div[1]/form[1]/div[1]/div[3]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/input[1]"
+                    },
+                    {
+                      "type": "xpath:position",
+                      "value": "//div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/input[1]"
+                    },
+                    {
+                      "type": "xpath:attributes",
+                      "value": "//input[@id='input-778']"
+                    },
+                    {
+                      "type": "css:position",
+                      "value": "css=#input-778"
+                    }
+                  ],
+                  "tac": ""
+                },
+                "value": {
+                  "usedIndex": 0,
+                  "options": [
+                    {
+                      "type": "other",
+                      "value": "130,16"
+                    }
+                  ],
+                  "tac": ""
+                },
+              
+              },
+              {
+                "name": "sendKeys",
+                "target": {
+                  "usedIndex": 1,
+                  "options": [
+                    {
+                      "type": "id",
+                      "value": "id=input-778"
+                    },
+                    {
+                      "type": "xpath:idRelative",
+                      "value": "//div[@id='app']/div[3]/div[1]/div[1]/div[1]/form[1]/div[1]/div[3]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/input[1]"
+                    },
+                    {
+                      "type": "xpath:position",
+                      "value": "//div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/input[1]"
+                    },
+                    {
+                      "type": "xpath:attributes",
+                      "value": "//input[@id='input-778']"
+                    },
+                    {
+                      "type": "css:position",
+                      "value": "css=#input-778"
+                    }
+                  ],
+                  "tac": ""
+                },
+                "value": {
+                  "usedIndex": 0,
+                  "options": [
+                    {
+                      "type": "other",
+                      "value": "Test:Dashlet"
+                    }
+                  ],
+                  "tac": ""
+                },
+                
+              },
+              {
+                "name": "clickAt",
+                "target": {
+                  "usedIndex": 1,
+                  "options": [
+                    {
+                      "type": "id",
+                      "value": "id=input-783"
+                    },
+                    {
+                      "type": "xpath:idRelative",
+                      "value": "//div[@id='app']/div[3]/div[1]/div[1]/div[1]/form[1]/div[1]/div[3]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/textarea[1]"
+                    },
+                    {
+                      "type": "xpath:position",
+                      "value": "//textarea[1]"
+                    },
+                    {
+                      "type": "xpath:attributes",
+                      "value": "//textarea[@id='input-783']"
+                    },
+                    {
+                      "type": "css:position",
+                      "value": "css=#input-783"
+                    }
+                  ],
+                  "tac": ""
+                },
+                "value": {
+                  "usedIndex": 0,
+                  "options": [
+                    {
+                      "type": "other",
+                      "value": "160,26"
+                    }
+                  ],
+                  "tac": ""
+                },
+               
+              },
+              {
+                "name": "clickAt",
+                "target": {
+                  "usedIndex": 0,
+                  "options": [
+                    {
+                      "type": "xpath:idRelative",
+                      "value": "//div[@id='app']/div[3]/div[1]/div[1]/div[2]/button[2]/span[1]"
+                    },
+                    {
+                      "type": "xpath:position",
+                      "value": "//div[1]/div[1]/div[2]/button[2]/span[1]"
+                    },
+                    {
+                      "type": "css:position",
+                      "value": "css=button.v-btn.v-btn--text.theme--light.v-size--default.primary--text > span.v-btn__content"
+                    }
+                  ],
+                  "tac": ""
+                },
+                "value": {
+                  "usedIndex": 0,
+                  "options": [
+                    {
+                      "type": "other",
+                      "value": "64,8"
+                    }
+                  ],
+                  "tac": ""
+                },
+                
+              }
+            ]
+          }
+        ]
+      }
+    ]
+};
+obj=JSON.stringify(obj);
+console.log(obj);
+sideex.setting.setSpeed(0);
+sideex.file.testSuite.load(obj);
+sideex.playback.start();
+for(var i=0;i<12;i++){
+  console.log(sideex.file.command.get(i),i);
+}
+/*document.getElementById("r.start").onclick=function(){
+      sideex.recorder.start();
+};
+document.getElementById("r.stop").onclick=function(){
+      sideex.recorder.stop();
+      console.log(sideex.file.testSuite.save());
+      let jsonString = sideex.file.testSuite.save();
+      console.log(jsonString);
+};
+document.getElementById("start").onclick=function(){
+      sideex.playback.start();
+};*/
+
+
+
+
+
+
+
+
+
+
